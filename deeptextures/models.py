04bf35720535a0f4af2ad8c3dc106e9d8ee3bed8
ch00cy <chooyeonhee99@gmail.com>
2023-03-23 06:17:44 +0900

    plus

--- deeptextures/models.py
+++ deeptextures/models.py
@@ -2,16 +2,32 @@
 """
 import torch
 import torch.nn as nn
-from torch.nn.functional import relu
+# torch.nn : 인스턴스화 시켜야함 -> attribute(클래스 내부에 포함되어있는 메소드,변수) 활용해 state 저장 가능
+# torch.nn.fuctional : 인스턴스화 시킬 필요 없이 바로 입력값 받을 수 있음
+# 인스턴스 : 클래스 -> 객체 -> 실체화 => 인스턴스
+from torch.nn.functional import relu    # relu 요소 관련
 #from torchvision.models.utils import load_state_dict_from_url
 from torch.hub import load_state_dict_from_url  #에러대체
+# Pytorch Hub-> 연구 재현성을 촉진하도록 설계된 사전 훈련된 모델 리포지토리
+# load_state_dict_from_url : 주어진 URL에서 Torch 직렬화된 개체를 로드 -> Dict[str, Any] 반환
 from torchvision.models.vgg import cfgs, make_layers, model_urls
+# cfg 들: GG 모델의 구조 정보 (각 vgg 모델마다 input, output 개수, 레이어 개수, pooling 언제 하는지 정보 들어있음)
+'''
+cfgs: Dict[str, List[Union[str, int]]] = {
+    "A": [64, "M", 128, "M", 256, 256, "M", 512, 512, "M", 512, 512, "M"],
+    "B": [64, 64, "M", 128, 128, "M", 256, 256, "M", 512, 512, "M", 512, 512, "M"],
+    "D": [64, 64, "M", 128, 128, "M", 256, 256, 256, "M", 512, 512, 512, "M", 512, 512, 512, "M"],
+    "E": [64, 64, "M", 128, 128, "M", 256, 256, 256, 256, "M", 512, 512, 512, 512, "M", 512, 512, 512, 512, "M"],
+}
+'''
+# make_layers : conv2d relu poll 등 레이어 만들기
+# model_urls : vgg 11 ~ 19 모델들에 대한 정보 url 들 담음
 
 
-class VGG19(nn.ModuleDict):
-    def __init__(self, avg_pool=True):
-        super().__init__()
-        self.avg_pool = avg_pool
+class VGG19(nn.ModuleDict): # torch.nn.ModuleDict : Model dictionary - (string: module) 매핑, 키-값 쌍의 iterable
+    def __init__(self, avg_pool=True):  # 초기화 함수
+        super().__init__()  # 부모 클래스 의 __init__ 불러옴 / 부모클래스 -> nn.ModuleDict 로 추정
+        self.avg_pool = avg_pool    # 클래스의 변수
 
         self.layer_names = """
         conv1_1 conv1_2 pool1
